95e330fd755ba6f850517a4c385908fc83868777
hrithik86 <hrithikmalik86@gmail.com>
2020-01-12 16:33:15 +0530

    Numbers

---
+++ numbers.py
@@ -0,0 +1,15 @@
+#simple operations
+print(2+10)
+#power  
+print(2**10)
+#variable assignment
+a=5
+print(a)
+print(a+a)
+a=a*5
+print(a)
+#snake_casing in variable name(not camelCasing)
+my_income=100
+tax=0.1
+my_taxes=my_income*tax
+print(my_taxes)
