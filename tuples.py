59e401d6015584bc42d5655f61d2b1841e52fcad
hrithik86 <hrithikmalik86@gmail.com>
2020-01-22 11:32:51 +0530

    added tuple unpacking

--- tuples.py
+++ tuples.py
@@ -5,3 +5,11 @@ print(t[0])
 t=('a',True,123)
 #tuples are same as list but they are immutable
 #t[0]='NEW'  #gives error
+
+#   Tuple Unpacking
+x=[(1,2),(3,4),(5,6)]
+for (a,b) in x:
+    print(a)
+#1
+#3
+#5
